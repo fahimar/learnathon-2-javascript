9cfdb39abb03c9f34ad2c3a4a269eef9ff7d0e25
fahimar <fahimar14040@gmail.com>
2023-09-15 21:11:53 +0600

    Some array with method.

--- class-2/Array.js
+++ class-2/Array.js
@@ -1,1 +1,18 @@
 const cars = ["Saab", "Volvo", "BMW"];
+
+const cars2 = [
+    "Mercedes-Benz",
+    "Ford",
+    "Chevrolet",
+    "Dodge",
+    "Ferrari",
+    "Jaguar"
+];
+
+let size = cars2.length;
+
+console.log(size);
+
+cars2.sort();
+cars2.reverse();
+console.log(cars2);
