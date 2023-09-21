b0cce5a930723dbe95a63515152575e949cf42b5
fahimar <fahimar14040@gmail.com>
2023-09-22 02:23:08 +0600

    DOM class 3 object consept commited

--- class-3/object.js
+++ class-3/object.js
@@ -4,3 +4,8 @@ console.dir(document.getElementById("demo"));
 const p = document.getElementById("demo");
 
 p.innerHTML = "Test 4";
+document.getElementsByTagName("p");
+
+document.getElementsByTagName("a");
+document.getElementsByClassName("anchor");
+document.getElementById("anchor").innerHTML = "Akil";
