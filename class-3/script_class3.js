caf4ef4fdc1ddfb8de9dd6d3779a5c4ca9cd3a38
fahimar <fahimar14040@gmail.com>
2023-09-21 19:07:30 +0600

    commited class-3

---
+++ class-3/script_class3.js
@@ -0,0 +1,9 @@
+// Remember, gonna use strict mode in all scripts now!
+"use strict";
+
+const x = "23";
+if (x === 23) console.log(23);
+
+const calAge = (birthYear) => 2023 - birthYear;
+//console.log();
+console.log(x);
