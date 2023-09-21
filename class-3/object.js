1ecbe56ecc5f3ac5a5ec5b4eb9580fb4f3fc563a
fahimar <fahimar14040@gmail.com>
2023-09-22 01:56:55 +0600

    DOM class 3

---
+++ class-3/object.js
@@ -0,0 +1,6 @@
+//P O E M              ***************** DOM **************************
+//properties Objects Events Methods
+console.dir(document.getElementById("demo"));
+const p = document.getElementById("demo");
+
+p.innerHTML = "Test 4";
