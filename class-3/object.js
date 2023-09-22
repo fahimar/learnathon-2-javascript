34faf70d4399c1e0db522ecfd3f9e3975f1fa288
fahimar <fahimar14040@gmail.com>
2023-09-22 12:04:34 +0600

    finding html elements by HTML & css object

--- class-3/object.js
+++ class-3/object.js
@@ -9,3 +9,11 @@ document.getElementsByTagName("p");
 document.getElementsByTagName("a");
 document.getElementsByClassName("anchor");
 document.getElementById("anchor").innerHTML = "Akil";
+
+document.querySelector("#demo");
+document.querySelector(".anchor");
+document.querySelectorAll(".anchor");
+
+// finding html elements by HTML object Collections
+
+document.getElementById("demo").innerHTML = text;
