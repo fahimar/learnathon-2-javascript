1bdb4e6bc77a219cbb606bf60de2981940f233af
fahimar <fahimar14040@gmail.com>
2023-10-01 23:31:02 +0600

    Abstraction with Class.

--- class-5/abstraction.js
+++ class-5/abstraction.js
@@ -18,3 +18,24 @@ Teacher.prototype = Object.create(Person.prototype);
 var teacher1 = new Teacher("M. Firoz Ahmed");
 console.log(teacher1.info());
 // Person name is: M. Firoz Ahmed    is the console output
+// By utilizing the “class” keyword
+class PersonClass {
+  constructor() {
+    if (this.constructor == Person) {
+      throw new Error("Your Error Message...");
+    }
+  }
+  info() {
+    throw new Error(" Added abstract Method has no implementation");
+  }
+}
+
+class TeacherClass extends PersonClass {
+  info() {
+    console.log("I am a Student");
+  }
+}
+
+var teacher2 = new TeacherClass();
+teacher2.info();
+// console.log(teacher2.info());
