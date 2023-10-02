4d832fcf72fc63e3e278f8d02c394bd0aae81141
fahimar <fahimar14040@gmail.com>
2023-10-02 23:45:01 +0600

    Advanced DOM Event commited.

---
+++ class-3/Advanced_DOM_script.js
@@ -0,0 +1,9 @@
+//////////////////////////
+//////////////////////////
+console.log(decument.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
