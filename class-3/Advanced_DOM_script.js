844ef214ed1c5c3493cc967ece05622a5f7fc016
fahimar <fahimar14040@gmail.com>
2023-10-03 00:44:34 +0600

    Dom manipulation.

--- class-3/Advanced_DOM_script.js
+++ class-3/Advanced_DOM_script.js
@@ -1,9 +1,13 @@
 //////////////////////////
-//////////////////////////
-console.log(decument.documentElement);
+// Selecting elements
+console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
 document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
 console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
