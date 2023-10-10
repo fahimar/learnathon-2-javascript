dcf23fa11414599bdc4529a041ca2f681942ab58
fahimar <fahimar14040@gmail.com>
2023-10-10 23:36:58 +0600

    Object in JS.

---
+++ class-5/object.js
@@ -0,0 +1,16 @@
+let name = "Mosh";
+let age = 30;
+
+let person = {
+  name: "Fahim",
+  age: 24,
+};
+
+// Dot Notation
+person.name = "Akil";
+
+// Bracket Notation
+let selection = "name";
+person[selection] = "Al Rashid";
+
+console.log(person.name);
