504d27517a7c8e56b35477f5f64a7f8d77bd7db3
fahimar <fahimar14040@gmail.com>
2023-10-13 00:34:10 +0600

    Class in commited.

--- class-5/class.js
+++ class-5/class.js
@@ -1,1 +1,24 @@
-// what is factory pattern
+class Person {
+  constructor(name, age, year) {
+    this.name = name;
+    this.age = age;
+  }
+  play() {
+    console.log(`${this.name} is playing.`);
+  }
+  ageExp() {
+    let date = new Date();
+    return this.year - this.age;
+  }
+}
+
+const person1 = new Person("Shanto", 28, 2018); // create actual Object
+const person2 = new Person("Afif", 24, 2020);
+const person3 = new Person("Mushfiqur", 36, 2001);
+
+person1.play();
+console.log(`His current experience is ${person1.age}`);
+person2.play();
+console.log(`His current experience is ${person2.age}`);
+person3.play();
+console.log(`His current experience is ${person3.age}`);
