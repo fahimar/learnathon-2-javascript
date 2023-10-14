786010885c344ef32f9e640faa0d2e68be609b28
fahimar <fahimar14040@gmail.com>
2023-10-14 12:59:17 +0600

    Asynchronous ajax commited.

--- class-4/Asynchronous.js
+++ class-4/Asynchronous.js
@@ -5,3 +5,9 @@ img.addEventListener("load", function () {
   img.classList.add("fadeIn");
 });
 p.style.width = "300px";
+
+const p = document.querySelector(".p");
+setTimeout(function () {
+  p.textContent = "My name is Fahim!";
+}, 5000);
+p.style.color = "red";
