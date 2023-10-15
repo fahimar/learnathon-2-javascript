4d2969557eaa4d2221ef08c9fdd964ec3dca91ab
fahimar <fahimar14040@gmail.com>
2023-10-15 15:43:35 +0600

    Promises commited.

--- class-4/promises.js
+++ class-4/promises.js
@@ -63,6 +63,16 @@ console.log(request);
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      //  return 23;
+    })
+    .then((response) => response.json())
+    .theb((data) => renderCountry(data, "neighbour"));
 };
 getCountryData("bangladesh");
