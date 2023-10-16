0aaf6ce1688383c5a6df4efdf72c73c20cc83cb9
fahimar <fahimar14040@gmail.com>
2023-10-17 00:56:25 +0600

    Count Occurence commited.

---
+++ Data Structures & Algorithms/week - 1/3-count-occurences/count-occurrences-run.js
@@ -0,0 +1,8 @@
+// const countOccurrences = require("./count-occurrences");
+const countOccurrences_2 = require("./count-occurrences");
+
+// const result = countOccurrences("hello world", "l");
+const result2 = countOccurrences_2("hellLLo world", "o");
+
+// console.log(result);
+console.log(result2);
--- Data Structures & Algorithms/week - 1/3-count-occurences/count-occurrences.js
+++ Data Structures & Algorithms/week - 1/3-count-occurences/count-occurrences.js
@@ -2,8 +2,19 @@ function countOccurrences(str, char) {
   let count = 0;
   let len = str.length;
   for (let i = 0; i < len; i++) {
-    console.log(str[i]);
+    // console.log(str[i]);
+    if (str[i] === char) {
+      count++;
+    }
   }
+
+  return count;
 }
 
-module.exports = countOccurrences;
+const countOccurrences_2 = (str, char) => {
+  //   console.log(str.split(char).length - 1);
+  return str.split(char).length - 1;
+};
+
+// module.exports = countOccurrences;
+module.exports = countOccurrences_2;
