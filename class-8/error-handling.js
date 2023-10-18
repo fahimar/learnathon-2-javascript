adf1e255636c0bf17070d65e69ab271a859eadd0
fahimar <fahimar14040@gmail.com>
2023-10-18 23:23:39 +0600

    Error Handling , different type of error in JS.

---
+++ class-8/error-handling.js
@@ -0,0 +1,36 @@
+console.log("Welcome to Class-8");
+
+/**
+ * Different types of error in JS
+ */
+
+//! Syntax Error
+// if (true)
+//   console.log("This is a syntax error");
+// }
+
+// const arr = [10,20,30];
+// for(const num = 50 of nums){
+//    console.log(num);
+// }
+
+//! Reference Error
+// console.log(vivasoft);
+
+// function foo() {
+//   "use strict";
+//   bar = "vivasoft";
+// }
+
+// foo();
+
+//! Type Error
+// let num = 7;
+// num.split();
+
+const myLuckyNumber = 999;
+myLuckyNumber = 777;
+
+//! Range Error
+const myArr = new Array(5);
+console.log(myArr);
