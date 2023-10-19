77ce537c5a1a5b85054a1d93efb0704bea6a6704
fahimar <fahimar14040@gmail.com>
2023-10-20 02:19:26 +0600

    ReverseString commited (DataStructure).

---
+++ Data Structures & Algorithms/week - 1/6-reverse-string/reverse-string.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
+  }
+  return rev;
+  //   return str.split("").reverse().join("");
+}
+
+module.exports = reverseString;
