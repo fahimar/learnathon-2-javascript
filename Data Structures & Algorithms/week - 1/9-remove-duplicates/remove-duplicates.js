1e631c650d7fc60eedd306b62371c5b1c638ebc2
fahimar <fahimar14040@gmail.com>
2023-10-26 23:37:23 +0600

    remove duplicate using set.

--- Data Structures & Algorithms/week - 1/9-remove-duplicates/remove-duplicates.js
+++ Data Structures & Algorithms/week - 1/9-remove-duplicates/remove-duplicates.js
@@ -1,13 +1,19 @@
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueArr.includes(arr[i])) {
+//       uniqueArr.push(arr[i]);
+//     }
+//   }
+
+//   return uniqueArr;
+// }
+// using set function
+
 function removeDuplicates(arr) {
-  const uniqueArr = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (!uniqueArr.includes(arr[i])) {
-      uniqueArr.push(arr[i]);
-    }
-  }
-
-  return uniqueArr;
+  console.log(arr);
+  return Array.from(new Set(arr));
 }
 
 module.exports = removeDuplicates;
