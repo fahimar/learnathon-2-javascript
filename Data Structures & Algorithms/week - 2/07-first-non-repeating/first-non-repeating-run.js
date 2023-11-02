84b7dcc54f3902eb173200265d04bebf55500b09
fahimar <fahimar14040@gmail.com>
2023-11-02 16:49:41 +0600

    # Challenge: Find First Non-Repeating Character. commited. This challenge is similar to the last. So if you understood that, you should be able to get this one on your own.

---
+++ Data Structures & Algorithms/week - 2/07-first-non-repeating/first-non-repeating-run.js
@@ -0,0 +1,7 @@
+const findFirstNonRepeatingCharacter = require("./first-non-repeating");
+
+const result1 = findFirstNonRepeatingCharacter("progrmming");
+const result2 = findFirstNonRepeatingCharacter("aabbcddeff");
+
+console.log(result1);
+console.log(result2);
---
+++ Data Structures & Algorithms/week - 2/07-first-non-repeating/first-non-repeating.js
@@ -0,0 +1,36 @@
+// Solution 1
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  // console.log(charCount);
+
+  return null;
+}
+// Solution 2
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = new Map();
+
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // console.log(charCount);
+
+  return null;
+}
+
+module.exports = findFirstNonRepeatingCharacter;
