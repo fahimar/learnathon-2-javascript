43f0f7052f7a54b1276fe81995f0950a0b1bf97e
fahimar <fahimar14040@gmail.com>
2023-11-15 19:55:45 +0600

    Higest scoring words using map() and reduce() ,The reduce() method is an iterative method. It runs a "reducer" callback function over all elements in the array, in ascending-index order, and accumulates them into a single value.

--- Data Structures & Algorithms/week-3-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ Data Structures & Algorithms/week-3-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,25 +1,41 @@
+// function highestScoringWord(str) {
+//   const words = str.split(" ");
+
+//   const scores = words.map((word) => {
+//     let score = 0;
+//     for (const letter of word) {
+//       score += letter.charCodeAt(0) - 96;
+//     }
+//     return score;
+//   });
+
+//   //   return scores;
+//   let highestScore = 0;
+//   let highestIndex = 0;
+
+//   for (let i = 0; i < scores.length; i++) {
+//     if (scores[i] > highestScore) {
+//       highestScore = scores[i];
+//       highestIndex = i;
+//     }
+//   }
+//   return words[highestIndex];
+// }
+
 function highestScoringWord(str) {
   const words = str.split(" ");
 
   const scores = words.map((word) => {
-    let score = 0;
-    for (const letter of word) {
-      score += letter.charCodeAt(0) - 96;
-    }
-    return score;
+    return Array.from(word).reduce(
+      (score, letter) => score + letter.charCodeAt(0) - 96,
+      0
+    );
   });
 
   //   return scores;
-  let highestScore = 0;
-  let highestIndex = 0;
+  let highestScore = Math.max(...scores);
+  let highestIndex = scores.indexOf(highestScore);
 
-  for (let i = 0; i < scores.length; i++) {
-    if (scores[i] > highestScore) {
-      highestScore = scores[i];
-      highestIndex = i;
-    }
-  }
   return words[highestIndex];
 }
-
 module.exports = highestScoringWord;
