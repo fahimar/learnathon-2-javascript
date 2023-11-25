20ac5c5d758f86e724726818842d69f65d69a493
fahimar <fahimar14040@gmail.com>
2023-11-25 23:55:29 +0600

    password validation (DataStructure) commited.

---
+++ Data Structures & Algorithms/week-3-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -0,0 +1,7 @@
+const validatePassword = require("./password-validator");
+
+const result1 = validatePassword("Abc12345");
+const result2 = validatePassword("password");
+const result3 = validatePassword("fahimabc@12345");
+
+console.log(result1, result2, result3);
