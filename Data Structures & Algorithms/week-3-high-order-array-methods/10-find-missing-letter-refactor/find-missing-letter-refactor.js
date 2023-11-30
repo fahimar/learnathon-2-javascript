bfb029e99e90cac4547c01717f196f4ac04de094
fahimar <fahimar14040@gmail.com>
2023-11-30 12:47:01 +0600

    Find Missing Letter Refactor commited.

--- Data Structures & Algorithms/week-3-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ Data Structures & Algorithms/week-3-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,18 +1,36 @@
-function findMissingLetter(arr) {
-  let start = arr[0].charCodeAt(0);
-  // return start;
+// function findMissingLetter(arr) {
+//   let start = arr[0].charCodeAt(0);
+//   // return start;
 
-  const missingCharCode = arr
-    .map((char) => char.charCodeAt(0))
-    .find((current) => {
-      if (current - start > 1) {
-        return true;
-      }
-      start = current;
+//   const missingCharCode = arr
+//     .slice(1);
+//     .map((char) => char.charCodeAt(0))
+//     .find((current) => {
+//       if (current - start > 1) {
+//         return true;
+//       }
+//       start = current;
+//       return false;
+//     });
+//   // return missingCharCode;
+//   return missingCharCode ? String.fromCharCode(missingCharCode - 1) : "";
+// }
+
+function findMissingLetter(arr) {
+  const missingCharCode = arr.filter((char, index) => {
+    if (index === 0) {
       return false;
-    });
-  // return missingCharCode;
-  return missingCharCode ? String.fromCharCode(missingCharCode - 1) : "";
+    }
+    const prevCharCode = arr[index - 1].charCodeAt(0);
+
+    const currentCharCode = char.charCodeAt(0);
+
+    return currentCharCode - prevCharCode > 1;
+  })[0];
+
+  return missingCharCode
+    ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1)
+    : "";
 }
 
 module.exports = findMissingLetter;
