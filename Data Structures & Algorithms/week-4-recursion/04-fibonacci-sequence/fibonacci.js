23eb0e648d24b8b80e13eb934fcd5498501b43a5
fahimar <fahimar14040@gmail.com>
2023-12-06 20:05:01 +0600

    Reverse string , fibonacci series commited.

---
+++ Data Structures & Algorithms/week-4-recursion/04-fibonacci-sequence/fibonacci.js
@@ -0,0 +1,13 @@
+// function fibonacci(num) {
+//   console.log(`Calculating fibonacci(${num})`);
+//   if (num < 2) {
+//     return num;
+//   }
+
+//   return fibonacci(num - 1) + fibonacci(num - 2);
+// }
+
+const fibonacci = (num) =>
+  num < 2 ? num : fibonacci(num - 1) + fibonacci(num - 2);
+
+module.exports = fibonacci;
