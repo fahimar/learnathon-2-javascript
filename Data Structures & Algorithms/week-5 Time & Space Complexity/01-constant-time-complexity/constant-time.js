f50e247c5cfc1fb1d450b5bfac64a8542d8a931e
fahimar <fahimar14040@gmail.com>
2023-12-13 22:30:29 +0600

    Time complexity linear commited.

--- Data Structures & Algorithms/week-5 Time & Space Complexity/01-constant-time-complexity/constant-time.js
+++ Data Structures & Algorithms/week-5 Time & Space Complexity/01-constant-time-complexity/constant-time.js
@@ -15,7 +15,7 @@ console.time("Access Element 1");
 console.log(accessElement(arr1, 1));
 console.timeEnd("Access Element 1");
 
-const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1);
+const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1); // '_' is place holder
 // console.log(arr2);
 console.time("Access Element 2");
 console.log(accessElement(arr2, 1000));
