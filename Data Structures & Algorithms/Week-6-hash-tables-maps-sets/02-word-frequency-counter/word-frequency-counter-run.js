da6de96072ecde55fd71a6e0d5c4a023cc824bb3
fahimar <fahimar14040@gmail.com>
2023-12-18 10:23:42 +0600

    word frequency counter using map datastructure commited.

---
+++ Data Structures & Algorithms/Week-6-hash-tables-maps-sets/02-word-frequency-counter/word-frequency-counter-run.js
@@ -0,0 +1,7 @@
+const wordfrequencyCounter = require("./word-frequency-counter");
+
+const result = wordfrequencyCounter(
+  "The quick brown fox jumps over the lazy dog."
+);
+
+console.log(result);
---
+++ Data Structures & Algorithms/Week-6-hash-tables-maps-sets/02-word-frequency-counter/word-frequency-counter.js
@@ -0,0 +1,17 @@
+function wordfrequencyCounter(str) {
+  const words = str.toLowerCase().split(/\W+/);
+  //   console.log(words);
+  const wordFrequency = new Map();
+  for (const word of words) {
+    if (word === "") continue;
+    if (wordFrequency.has(word)) {
+      wordFrequency.set(word, wordFrequency.get(word) + 1);
+    } else {
+      wordFrequency.set(word, 1);
+    }
+  }
+
+  return wordFrequency;
+}
+
+module.exports = wordfrequencyCounter;
