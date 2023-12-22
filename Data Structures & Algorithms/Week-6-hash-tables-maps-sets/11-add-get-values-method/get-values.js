e14060177fa36368054be66012b49d447e0a145c
fahimar <fahimar14040@gmail.com>
2023-12-22 19:00:00 +0600

    getValue method commited.

---
+++ Data Structures & Algorithms/Week-6-hash-tables-maps-sets/11-add-get-values-method/get-values.js
@@ -0,0 +1,16 @@
+class HashTable {
+  getValues() {
+    const values = [];
+
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i]) {
+        for (const [key, value] of this.storage[i]) {
+          values.push(value);
+        }
+      }
+    }
+    return values;
+  }
+}
+
+module.exports = HashTable;
