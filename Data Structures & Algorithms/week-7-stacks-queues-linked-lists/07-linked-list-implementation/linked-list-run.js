d90ff43c4b3b08300a594372d1be5e5d17bd51dd
fahimar <fahimar14040@gmail.com>
2023-12-28 23:25:57 +0600

    Linkdelist implementation commited.

---
+++ Data Structures & Algorithms/week-7-stacks-queues-linked-lists/07-linked-list-implementation/linked-list-run.js
@@ -0,0 +1,17 @@
+const { LinkedList } = require("./linked-list");
+
+const list = new LinkedList();
+
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+
+list.insertAt(1, "a");
+list.insertAt(3, "a");
+
+list.removeFrom(1);
+
+// console.log(list.get(2));
+// console.log(list.get(0));
+list.printAll();
